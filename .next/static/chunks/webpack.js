7c2a0e36a65260496120734801f1c68fe1b7f52f
Gabriel Henrique Chaves <ghchaves@msn.com>
2024-07-01 00:45:19 +0000

    secund commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "72f78bdf61bcf4d7"; }
+/******/ 		__webpack_require__.h = function() { return "6464fbe6c41b314c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Eu te amo S2</h1>
+  return <h1>Eu te amo demais S2</h1>
 }
 
 export default Home;
